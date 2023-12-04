77155d60cec9a555deb9b5e46a3fa3f111427c71
ItsKristian01 <kristianhana11@gmail.com>
2023-12-04 12:17:12 +0100

    add: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import mainPage from './components/mainPage.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+
+  components: {AppHeader, mainPage, AppFooter }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <MainPage />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css";
+</style>
