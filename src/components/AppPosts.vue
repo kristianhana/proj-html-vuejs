5b683a1f0037bc94a3bd91a9b465010d994da0f3
ItsKristian01 <kristianhana11@gmail.com>
2023-12-06 12:39:30 +0100

    update: posts section added badges

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -3,52 +3,55 @@
 
 <template>
     <section>
-    <div class="container">
-        <!-- Title -->
-        <div class="title text-center">
-            <h2 class="mb-4">Latest Posts</h2>
-            <p>If you are planning on developing a product landing</p>
-        </div>
-        <!-- Cards -->
-        <div class="row row-cols-3">
-            <div class="col">
-                <div class="card p-2">
-                    <img src="../assets/img/19.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="mb-3">Life Lack Meaning</h5>
-                        <p class="card-text">
-                            Its a long established fact that a reader will be distracted by the readable.
-                        </p>
-                    </div>
-                </div>
+        <div class="container">
+            <!-- Title -->
+            <div class="title text-center">
+                <h2 class="mb-4">Latest Posts</h2>
+                <p>If you are planning on developing a product landing</p>
             </div>
             <!-- Cards -->
-            <div class="col">
-                <div class="card p-2">
-                    <img src="../assets/img/18.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="mb-3">Life Lack Meaning</h5>
-                        <p class="card-text">
-                            Its a long established fact that a reader will be distracted by the readable.
-                        </p>
+            <div class="row row-cols-3">
+                <div class="col">
+                    <div class="card p-2">
+                        <img src="../assets/img/19.jpg" class="card-img-top mb-3" alt="">
+                        <span class="badge">November 28, 2019</span>
+                        <div class="card-body">
+                            <h5 class="mb-4">Life Lack Meaning</h5>
+                            <p class="card-text">
+                                Its a long established fact that a reader will be distracted by the readable.
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- Cards -->
-            <div class="col">
-                <div class="card p-2">
-                    <img src="../assets/img/17.jpg" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="mb-3">Life Lack Meaning</h5>
-                        <p class="card-text">
-                            Its a long established fact that a reader will be distracted by the readable.
-                        </p>
+                <!-- Cards -->
+                <div class="col">
+                    <div class="card p-2">
+                        <img src="../assets/img/18.jpg" class="card-img-top mb-3" alt="">
+                        <span class="badge">November 28, 2019</span>
+                        <div class="card-body">
+                            <h5 class="mb-4">Life Lack Meaning</h5>
+                            <p class="card-text">
+                                Its a long established fact that a reader will be distracted by the readable.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Cards -->
+                <div class="col">
+                    <div class="card p-2">
+                        <img src="../assets/img/17.jpg" class="card-img-top mb-3" alt="">
+                        <span class="badge">November 28, 2019</span>
+                        <div class="card-body">
+                            <h5 class="mb-4">Life Lack Meaning</h5>
+                            <p class="card-text">
+                                Its a long established fact that a reader will be distracted by the readable.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 </template>
 
 <style scoped lang="scss">
@@ -59,10 +62,17 @@
         margin-bottom: 90px;
     }
 
-    .row {
-        width: 80%;
-        margin-left: 130px;
+    .badge {
+        width: 160px;
+        padding: 10px;
+        background-color: hsl(0deg 0% 100%);
+        color: hsl(204.71deg 8.13% 59.02%);
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        font-size: 15px;
     }
+
 }
 
 p {
